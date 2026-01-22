b64edcdde0f7391bd0d1986d8afdcff4ca8eb16c
Deploy Agent <deploy@example.com>
2026-01-22 10:03:40 -0900

    Use insecure static serving to bypass collectstatic issues on Railway

--- STOR/urls.py
+++ STOR/urls.py
@@ -4,12 +4,13 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import re_path
 from django.views.static import serve
+from django.contrib.staticfiles import views as static_views
 
 urlpatterns = [
     path('django-admin/', admin.site.urls),
     path('', include('car_stor.urls')),
     re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', static_views.serve, {'insecure': True}),
 ]
 
 if settings.DEBUG:
